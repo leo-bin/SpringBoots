4d3f22be7dc471eddec0064596fd1f55c3dfd644
LEEBIN <199900libin@163.com>
2020-06-15 14:57:36 +0800

    Initial commit

---
+++ redis/src/main/java/bins/springboot/redis/RedisApplication.java
@@ -0,0 +1,16 @@
+package bins.springboot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * redis项目
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
